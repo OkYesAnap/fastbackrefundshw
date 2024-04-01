aacb960c697b7410dc738e0efe3fcc93d554fbde
Panaseiko Andrii <andrii.panaseiko@akvelon.com>
2024-04-01 12:23:21 +0300

    Added HTML comment skipping algorithm

--- backend/parser.js
+++ backend/parser.js
@@ -1,9 +1,10 @@
 const parseCopyrights = (html) => {
 	let startIndex = html.lastIndexOf('©');
-	let endIndex = Math.min(html.indexOf('\\', startIndex), html.indexOf('<', startIndex))
+	let endIndex = Math.min(html.indexOf('\\', startIndex), html.indexOf('</', startIndex))
 
 	if (startIndex !== -1 && endIndex !== -1 && startIndex < endIndex) {
-		return html.substring(startIndex, endIndex );
+		const copyrights = html.substring(startIndex, endIndex;:::
+		return copyrights.replace(/<!--(.*?)-->/g, '')
 	} else {
 		return 'Copyrights is not found';
 	}
